50115cfdeb7c662b0a3b86010d869d996f7ed26c
alexpatapan <42231868+alexpatapan@users.noreply.github.com>
2019-12-28 19:48:22 +1000

    Add files via upload

--- src/components/links.js
+++ src/components/links.js
@@ -18,16 +18,16 @@ class Links extends React.Component {
       <Router>
       <header className="App-header">
       <header className="topBar">
-          <Button href="/" >Home</Button>
-          <Button href="/about">About Me</Button>
-          <Button href="/resume">Resume</Button>
+          <Button href="/"  >Home</Button>
+          <Button href="/about" >About Me</Button>
+          <Button href="/resume" >Resume</Button>
           <Button href="/socials">Socials</Button>
           
           </header></header>
         <Switch>
         <Route path="/about">
           <header className="welcome"> <p></p>
-          I'm studying CS and Economics at The Univeristy of Queensland. <p></p>
+          I'm studying Computer Science and Economics at The Univeristy of Queensland. <p></p>
           <header className="body-aboutme">
           I'm in my second last year of studying and I major in machine learning. I write a lot of Python, Java, and C. <br></br>
           Outside of coding, I love to play guitar.
@@ -38,14 +38,17 @@ class Links extends React.Component {
         <header className="App-header"><header className="socials"><SimpleList /></header></header>
         </Route>
         <Route path="/resume">
-        <header className="App-header"><header className="resume"><p></p>
-          Software engineer at FuelzFoods Pty Ltd <font size="2">(Dec 2019 - Current)</font>
+        <header className="App-header"><header className="resume"><p></p><br></br>
+        Software Engineer at Appello Software <font size="2">(Dec 2019 - Current)</font>
+        <header className="body-aboutme">Involves the development of web applications to meet client's needs.</header><p></p>
+
+          Software Engineer at FuelzFoods Pty Ltd <font size="2">(Dec 2019 - Current)</font>
           <header className="body-aboutme">I write backend software to solve optimisation issues in the supply chain process.</header>
           <p></p>Data Analyst at Kros Holdings Pty Ltd <font size="2">(Nov 2018 - Feb 2019)</font>
           <header className="body-aboutme">Involved the analysis of financial data to identify business growth trends.</header>
           
-          <br></br><br></br><p></p><p></p> I currently hold a GPA of 6.5/7.0 and have recieved two Dean's commendations for Academic Excellence.
-          <p></p>I am also a recognised Scholar of the Faculty of Engineering at UQ.</header>
+          <br></br>I currently hold a GPA of 6.5/7.0 and have received two Dean's commendations for Academic Excellence.
+          <p></p>I am also a recognised scholar of the Faculty of Engineering at UQ.</header>
           </header>
           
 
